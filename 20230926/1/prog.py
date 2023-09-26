cb0025140b0d03aaf2307c6e493b9a01dec5a5e7
Сергей Орлов <orlov.2000@mail.ru>
2023-09-26 14:23:42 +0300

    check from 2

--- 20230926/1/prog.py
+++ 20230926/1/prog.py
@@ -1,2 +1,2 @@
 m, n = eval(input())
-print(a := [i for i in range(m, n) if all([i % j != 0 for j in range(2, i)])])
+print(a := [i for i in range(max(2, m), n) if all([i % j != 0 for j in range(2, i)])])
